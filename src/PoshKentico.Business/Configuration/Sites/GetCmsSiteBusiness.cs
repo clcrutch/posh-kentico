dd2090e8f377ed40f252ff480ae6647b254ea80d
sophia.mao <sophia.mao@lightstream.com>
2018-07-23 16:57:13 -0700

    Resolve Warnings

--- src/PoshKentico.Business/Configuration/Sites/GetCmsSiteBusiness.cs
+++ src/PoshKentico.Business/Configuration/Sites/GetCmsSiteBusiness.cs
@@ -1,12 +1,26 @@
-﻿
-using PoshKentico.Core.Services.Configuration;
-using PoshKentico.Core.Services.General;
+﻿// <copyright file="GetCmsSiteBusiness.cs" company="Chris Crutchfield">
+// Copyright (C) 2017  Chris Crutchfield
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+// </copyright>
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PoshKentico.Core.Services.Configuration;
+using PoshKentico.Core.Services.General;
 
 namespace PoshKentico.Business.Configuration.Sites
 {
