6df51be8114f77424d86610f98de4ea559df44a4
sophiahuimao <sophiahuimao@gmail.com>
2018-09-19 14:35:56 -0700

    Enable-CMSUser and Disable-CMSUser

--- src/PoshKentico/PSCmdlets.psm1
+++ src/PoshKentico/PSCmdlets.psm1
@@ -17,4 +17,32 @@ function Set-CMSUserPrivilegeLevel {
     }
 }
 
+function Enable-CMSUser {
+	[CmdletBinding()]
+    param (
+		[parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [CMS.Membership.UserInfo]$User
+    )
+    PROCESS {
+        Write-Verbose "Starting cmdlet"
+		$User.Enabled = $true
+		$User| Set-CMSUser
+    }
+}
+
+function Disable-CMSUser {
+	[CmdletBinding()]
+    param (
+		[parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [CMS.Membership.UserInfo]$User
+    )
+    PROCESS {
+        Write-Verbose "Starting cmdlet"
+		$User.Enabled = $false
+		$User| Set-CMSUser
+    }
+}
+
 Export-ModuleMember -Function 'Set-CMSUserPrivilegeLevel'
+Export-ModuleMember -Function 'Enable-CMSUser'
+Export-ModuleMember -Function 'Disable-CMSUser'
