759b21ed16ae801e4af04ce4211d55d0090ec2f7
sophiahuimao <sophiahuimao@gmail.com>
2018-09-20 12:09:10 -0700

    more unit tests

---
+++ src/PoshKentico.Tests/Configuration/Users/RemoveCmsUserTests.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="RemoveCmsUserTests.cs" company="Chris Crutchfield">
+// Copyright (C) 2017  Chris Crutchfield
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+// </copyright>
+
+using System.Collections.Generic;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using PoshKentico.Business.Configuration.Users;
+using PoshKentico.Core.Services.Configuration.Users;
+
+namespace PoshKentico.Tests.Configuration.Users
+{
+    [TestFixture]
+    public class RemoveCmsUserTests
+    {
+        [Test]
+        public void RemoveCmsUserTest_Object()
+        {
+            var userServiceMock = new Mock<IUserService>();
+            bool shouldProcessCalled = false;
+
+            var userMock1 = new Mock<IUser>();
+            var userMock2 = new Mock<IUser>();
+
+            var businessLayer = new RemoveCmsUserBusiness()
+            {
+                WriteDebug = Assert.NotNull,
+                WriteVerbose = Assert.NotNull,
+
+                UserService = userServiceMock.Object,
+                ShouldProcess = (x, y) =>
+                {
+                    shouldProcessCalled = true;
+                    return true;
+                },
+            };
+
+            businessLayer.RemoveUsers(userMock1.Object);
+            userServiceMock.Verify(x => x.DeleteUser(userMock1.Object));
+
+            businessLayer.RemoveUsers(userMock2.Object);
+            userServiceMock.Verify(x => x.DeleteUser(userMock2.Object));
+
+            shouldProcessCalled.Should().BeTrue();
+        }
+    }
+}
---
+++ src/PoshKentico.Tests/Configuration/Users/RemoveUserFromSiteTests.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="RemoveUserFromSiteTests.cs" company="Chris Crutchfield">
+// Copyright (C) 2017  Chris Crutchfield
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+// </copyright>
+
+using Moq;
+using NUnit.Framework;
+using PoshKentico.Business.Configuration.Users;
+using PoshKentico.Core.Services.Configuration.Users;
+
+namespace PoshKentico.Tests.Configuration.Users
+{
+    [TestFixture]
+    public class RemoveUserFromSiteTests
+    {
+        [Test]
+        public void RemoveUserFromSiteTest()
+        {
+            var userServiceMock = new Mock<IUserService>();
+
+            var userMock1 = new Mock<IUser>();
+
+            var businessLayer = new RemoveCmsUserFromSiteBusiness()
+            {
+                WriteDebug = Assert.NotNull,
+                WriteVerbose = Assert.NotNull,
+
+                UserService = userServiceMock.Object,
+            };
+
+            businessLayer.RemoveUserFromSite(userMock1.Object, "test site");
+
+            userServiceMock.Verify(x => x.RemoveUserFromSite(userMock1.Object, "test site"));
+        }
+    }
+}
