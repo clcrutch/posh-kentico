def900114f5ac59d400a20fcee0d1f0bc0dc96e9
Chris Crutchfield <chris.crutchfield@outlook.com>
2018-09-28 11:27:50 -0700

    upgrade mock

--- src/PoshKentico.Tests/Configuration/Sites/SiteServiceMock.cs
+++ src/PoshKentico.Tests/Configuration/Sites/SiteServiceMock.cs
@@ -20,6 +20,7 @@ using System.Diagnostics.CodeAnalysis;
 using FluentAssertions;
 using Moq;
 using PoshKentico.Core.Services.Configuration.Localization;
+using PoshKentico.Core.Services.Configuration.ScheduledTasks;
 using PoshKentico.Core.Services.Configuration.Sites;
 
 namespace PoshKentico.Business.Configuration.Sites
@@ -110,6 +111,11 @@ namespace PoshKentico.Business.Configuration.Sites
         }
 
         public IEnumerable<ISiteDomainAlias> GetDomainAliases(ISite site)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISite GetSite(IScheduledTask scheduledTask)
         {
             throw new System.NotImplementedException();
         }
