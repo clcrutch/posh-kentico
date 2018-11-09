aeb9ea68f8f09cc3ee9e87af391ea6677c7e246c
Chris Crutchfield <chris.crutchfield@lightstream.com>
2018-11-09 09:35:12 -0800

    restore TestScript

--- src/PoshKentico/TestScript.ps1
+++ src/PoshKentico/TestScript.ps1
@@ -5,3 +5,61 @@ $DebugPreference = "Continue"
 
 $env:PSModulePath="$env:PSModulePath;..\"
 Import-Module posh-kentico
+
+Configuration KenticoTest
+{
+	Import-DscResource -Name xWebPartCategory
+	Import-DscResource -Name xWebPart
+	Import-DscResource -Name xSite
+	Import-DscResource -Name xServer
+	Import-DscResource -Name xSettingValue
+	Import-DscResource -Name xMediaLibrary
+
+	Node localhost
+	{
+		
+		xSite TestSite
+		{
+			SiteName = "Kenticotest"
+			DomainName = "localhost:743"
+			DisplayName = "KenticoTest"
+			Status = "Running"
+			DomainAlias = "127.0.0.1"
+			Ensure = "Present"
+		}
+
+		xServer TestServer
+		{
+			ServerName = "Kenticotest"
+			ServerSiteName = "Kenticotest"
+			ServerURL = "http://dappcluster:743"
+			ServerDisplayName = "KenticoTest"
+			ServerAuthentication = "UserName"
+			ServerEnabled = $true
+			ServerUsername = "admin"
+			ServerPassword = "pass"
+			Ensure = "Present"
+		}
+
+		xSettingValue TestSettingValue
+		{
+			Key = "CMSSchedulerTasksEnabled"
+			Value = "60"
+			SiteName = "Kenticotest"
+		}
+
+		xMediaLibrary TestMediaLibrary
+		{
+			LibraryName = "NewLibrary"
+			LibraryDisplayName = "New Library"
+			LibrarySiteName = "Kenticotest"
+			LibraryFolder = "New library"
+			LibraryDescription = "This media library was created through the DSC."
+			Ensure = "Present"
+		}
+	}
+}
+
+KenticoTest -OutputPath .\Temp
+
+Start-DscConfiguration -Wait -Force -Path .\Temp
